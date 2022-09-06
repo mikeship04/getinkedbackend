5cdbcab2bb7abc252a33f5edc6935b853937add0
Mike Shippy <mikeship04@gmail.com>
2022-09-06 11:08:14 -0700

    add Artist form complete

--- app/controllers/giveaways_controller.rb
+++ app/controllers/giveaways_controller.rb
@@ -1,8 +1,15 @@
 class GiveawaysController < ApplicationController
-    skip_before_action :authorized, only: [:create, :index]
+    skip_before_action :authorized, only: [:create, :index, :show]
 
     def index
         render json: Giveaway.all
+    end
+
+    def show
+        puts params
+        puts :artist_id
+        giveaway = Giveaway.find_by(params[:artist_id])
+        render json: giveaway
     end
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-    attributes :username, :email, :id
+    attributes :username, :email, :id, :admin
 end
