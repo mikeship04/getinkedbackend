56da278c3763c93b1c777c5d9238b37c22283e50
Mike Shippy <mikeship04@gmail.com>
2022-09-12 15:20:32 -0700

    added crud for artists and giveaways

--- app/controllers/giveaways_controller.rb
+++ app/controllers/giveaways_controller.rb
@@ -1,8 +1,26 @@
 class GiveawaysController < ApplicationController
-    skip_before_action :authorized, only: [:create, :index, :show]
+    skip_before_action :authorized, only: [:create, :index, :show, :update]
 
     def index
         render json: Giveaway.all
+    end
+
+    def show
+        artist = Artist.find_by(id: params[:id])
+        render json: artist.Giveaways
+    end
+
+    def update
+        puts params
+        artist = Artist.find(params[:id])
+        artist.Giveaways.update(giveaway_params)
+        render json: artist
+    end
+
+    private
+
+    def giveaway_params
+        params.require(:giveaway).permit(:header, :description, :closing_date, :img_url, :full_details)
     end
 
 end
